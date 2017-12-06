3590be418b71d16889e8d9531341d186d460d4a8
Northburns <northburns@gmail.com>
2017-12-06 10:57:23 +0200

    Day 6

---
+++ src/main/kotlin/aoc2017/Day6_MemoryReallocation.kt
@@ -0,0 +1,84 @@
+package aoc2017
+
+import kotlin.coroutines.experimental.buildIterator
+
+/*
+  Yikes, look at all those loops :)
+
+ */
+
+data class Block(
+        val location: Int)
+
+/**
+ * Indexes are zero based.
+ */
+data class Memory(
+        private val blocks: Collection<Block>,
+        private val bankCount: Int,
+        val redisributeCounter: Int = 0) {
+
+    private fun Int.nextBank() = (this + 1) % bankCount
+
+    constructor(bankCounts: List<Int>) : this(bankCounts
+            .mapIndexed { index, blockCount ->
+                Array(blockCount) { Block(index) }.toList()
+            }.flatten(), bankCounts.size)
+
+    fun bankWithMostBlocks(): Int = blocks
+            .groupBy { it.location }
+            .maxWith(compareBy(
+                    { it.value.size },
+                    { -it.key }))!!
+            .key
+
+    /**
+     * Creates a copy of self where memory has been redistributed.
+     */
+    fun redistribute(): Memory {
+
+        val iMax = bankWithMostBlocks()
+
+        val redistrBank = buildIterator {
+            var i = iMax
+            while (true) {
+                i = i.nextBank()
+                yield(i)
+            }
+        }
+        val redistributedBlocks = blocks
+                .map {
+                    it.copy(location = when (it.location) {
+                        iMax -> redistrBank.next()
+                        else -> it.location
+                    })
+                }
+
+        return copy(
+                blocks = redistributedBlocks,
+                redisributeCounter = redisributeCounter + 1)
+    }
+
+    /**
+     * @return second is the index of the memory state that equals to the last.
+     */
+    fun redistributionHistoryUntilLoops(): Pair<List<Memory>, Int> {
+        val history = mutableListOf(this to this.memoryBlockCounts())
+        var i: Int
+        do {
+            val m = history.last().first.redistribute().let { it to it.memoryBlockCounts() }
+            i = history.indexOfFirst { it.second == m.second }
+            history.add(m)
+        } while (i == -1)
+        return history.map { it.first } to i
+    }
+
+    /**
+     * Index matches memory bank index.
+     */
+    fun memoryBlockCounts() =
+            Array(bankCount) { i ->
+                blocks.count { it.location == i }
+            }.toList()
+
+}
---
+++ src/test/kotlin/aoc2017/Day6Test.kt
@@ -0,0 +1,45 @@
+package aoc2017
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day6Test {
+
+    private val input = inputFile("day6.txt").trim()
+            .split(Regex("\\s+"))
+            .map(String::toInt)
+            .toList()
+
+    @Test
+    fun examplesSolutions() {
+        var m = Memory(listOf(0, 2, 7, 0))
+
+        assertEquals(2, m.bankWithMostBlocks())
+        m = m.redistribute()
+        assertEquals(listOf(2, 4, 1, 2), m.memoryBlockCounts())
+
+        assertEquals(1, m.bankWithMostBlocks())
+        m = m.redistribute()
+        assertEquals(listOf(3, 1, 2, 3), m.memoryBlockCounts())
+
+        assertEquals(0, m.bankWithMostBlocks())
+        m = m.redistribute()
+        assertEquals(listOf(0, 2, 3, 4), m.memoryBlockCounts())
+
+        assertEquals(3, m.bankWithMostBlocks())
+        m = m.redistribute()
+        assertEquals(listOf(1, 3, 4, 1), m.memoryBlockCounts())
+
+        assertEquals(2, m.bankWithMostBlocks())
+        m = m.redistribute()
+        assertEquals(listOf(2, 4, 1, 2), m.memoryBlockCounts())
+
+        val (history, loopIndex) = Memory(input).redistributionHistoryUntilLoops()
+
+        println("Day 3, Part 1: ${history.last().redisributeCounter}")
+        println("Day 3, Part 2: ${history.size - loopIndex - 1}")
+
+        assertEquals(12841, history.last().redisributeCounter)
+    }
+
+}
