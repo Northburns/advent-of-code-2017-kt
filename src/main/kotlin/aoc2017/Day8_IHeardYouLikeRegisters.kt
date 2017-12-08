64aa0dfc7e3bae86e1fec65c2a374981b7902abb
Northburns <northburns@gmail.com>
2017-12-08 22:20:19 +0200

    Day 8

---
+++ src/main/kotlin/aoc2017/Day8_IHeardYouLikeRegisters.kt
@@ -0,0 +1,113 @@
+package aoc2017
+
+import aoc2017.common.computeIfAbsentK
+import kotlin.math.max
+
+
+enum class Compare(
+        private val string: String,
+        val p: (MutableRegister, Int) -> Boolean) {
+    LT("<", { r, i -> r.value < i }),
+    GT(">", { r, i -> r.value > i }),
+    LE("<=", { r, i -> r.value <= i }),
+    GE(">=", { r, i -> r.value >= i }),
+    EQ("==", { r, i -> r.value == i }),
+    NOT("!=", { r, i -> r.value != i });
+
+    companion object {
+        fun fromString(string: String) = values().find { it.string == string }!!
+    }
+}
+
+enum class Operation(
+        private val string: String,
+        val a: (MutableRegister, Int) -> Unit) {
+    INC("inc", { r, i -> r.value += i }),
+    DEC("dec", { r, i -> r.value -= i });
+
+    companion object {
+        fun fromString(string: String) = values().find { it.string == string }!!
+    }
+}
+
+data class Condition(
+        private val register: String,
+        private val compare: Compare,
+        private val value: Int) {
+    fun test(registry: Registry) = compare.p(registry.getRegister(register), value)
+
+
+}
+
+data class Instruction(
+        private val register: String,
+        private val operation: Operation,
+        private val value: Int,
+        private val `if`: Condition) {
+
+    fun executeOn(registry: Registry) {
+        if (`if`.test(registry)) operation.a(registry.getRegister(register), value)
+    }
+
+    companion object {
+        fun fromString(string: String): Instruction = string
+                .split(" if ")
+                .let { (instructionPart, conditionPart) ->
+                    val condition = conditionPart
+                            .split(" ")
+                            .let { (register, compare, value) ->
+                                Condition(
+                                        register,
+                                        Compare.fromString(compare),
+                                        value.toInt())
+                            }
+                    instructionPart
+                            .split(" ")
+                            .let { (register, operation, value) ->
+                                Instruction(
+                                        register,
+                                        Operation.fromString(operation),
+                                        value.toInt(),
+                                        condition)
+                            }
+
+                }
+    }
+}
+
+class Registry {
+
+    internal val registers = mutableMapOf<String, MutableRegister>()
+
+    private var instructions: List<Instruction> = emptyList()
+    private var head: Int = 0
+
+    private var highWaterMark = 0
+
+    fun reset(input: List<Instruction>) {
+        registers.clear()
+        instructions = input
+        head = 0
+        highWaterMark = Int.MIN_VALUE
+    }
+
+    fun step() {
+        instructions[head].executeOn(this)
+        head++
+        highWaterMark = max(highWaterMark, largestValue())
+    }
+
+    fun isDone() = head >= instructions.size
+
+    fun stepThrough() {
+        while (!isDone()) step()
+    }
+
+    internal fun getRegister(s: String) = registers.computeIfAbsentK(s) { MutableRegister() }
+    operator fun get(s: String) = getRegister(s).value
+    fun largestValue() = registers.values.maxBy { it.value }!!.value
+    fun largestValueEver() = highWaterMark
+
+}
+
+data class MutableRegister(var value: Int = 0)
---
+++ src/test/kotlin/aoc2017/Day8Test.kt
@@ -0,0 +1,55 @@
+package aoc2017
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class Day8Test {
+
+    private val input = inputFile("day8.txt")
+            .trim()
+            .lines().map(Instruction.Companion::fromString)
+
+    @Test
+    fun examplesPart1() {
+
+        val sampleInput = """
+            |b inc 5 if a > 1
+            |a inc 1 if b < 5
+            |c dec -10 if a >= 1
+            |c inc -20 if c == 10""".trimMargin()
+                .lines().map(Instruction.Companion::fromString)
+
+        val r = Registry()
+
+        r.reset(sampleInput)
+
+        r.step()
+        assertEquals(0, r["b"])
+
+        r.step()
+        assertEquals(1, r["a"])
+
+        r.step()
+        assertEquals(10, r["c"])
+
+        r.step()
+        assertEquals(-10, r["c"])
+
+        assertTrue { r.isDone() }
+
+        assertEquals(1, r.largestValue())
+        assertEquals(10, r.largestValueEver())
+
+        //
+
+        r.reset(input)
+        r.stepThrough()
+
+        println("Day 3, Part 1: ${r.largestValue()}")
+
+        println("Day 3, Part 2: ${r.largestValueEver()}")
+    }
+
+
+}
