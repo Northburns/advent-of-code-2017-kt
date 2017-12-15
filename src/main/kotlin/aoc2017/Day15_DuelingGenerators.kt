223c27fb6c56cc4685bc3fbf20971ebcc43711aa
Northburns <northburns@gmail.com>
2017-12-15 21:40:44 +0200

    Day 15.

---
+++ src/main/kotlin/aoc2017/Day15_DuelingGenerators.kt
@@ -0,0 +1,68 @@
+package aoc2017
+
+import aoc2017.common.Undefined
+
+class Generator(
+        private val name: String,
+        initialValue: Long,
+        private val rules: Rules,
+        val rememberValues: Boolean = false) {
+
+    enum class Rules(val valueOk: (String, Long) -> Boolean) {
+        PART1({ _, _ -> true }),
+        PART2({ name, value ->
+            when (name) {
+                "A" -> value % 4 == 0L
+                "B" -> value % 8 == 0L
+                else -> throw Undefined("")
+            }
+        })
+    }
+
+    companion object {
+        private val factors = mapOf(
+                "A" to 16807L,
+                "B" to 48271L)
+        private val div = 2147483647L
+
+        fun operation(name: String, previousValue: Long, rules: Rules): Long {
+            var v = previousValue
+            do {
+                v = v * factors[name]!! % div
+            } while (!rules.valueOk(name, v))
+            return v
+        }
+    }
+
+    var value: Long = operation(name, initialValue, rules)
+
+    val rememberedValues = mutableListOf(value)
+
+    fun step(): Long {
+        value = operation(name, value, rules)
+        if (rememberValues)
+            rememberedValues.add(value)
+        return value
+    }
+
+}
+
+class Judge(val gA: Generator, val gB: Generator) {
+
+    companion object {
+        val mask16bit = (1.shl(16) - 1).toLong()
+    }
+
+    fun step40MillionTimes() = stepNTimes(40_000_000L)
+    fun step5MillionTimes() = stepNTimes(5_000_000L)
+
+    fun stepNTimes(n: Long): Long {
+        var count = 0L
+        (0L..n).forEach {
+            val a = gA.step().and(mask16bit)
+            val b = gB.step().and(mask16bit)
+            if (a == b) count++
+        }
+        return count
+    }
+}
---
+++ src/test/kotlin/aoc2017/Day15Test.kt
@@ -0,0 +1,52 @@
+package aoc2017
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day15Test {
+
+    private val input = inputFile("day15.txt").trim().lines().map { line ->
+        val (g, v) = line.split("starts with")
+        g.split("Generator")[1].trim() to v.trim().toLong()
+    }.associateBy({ it.first }) { it.second }
+    private val sample = "flqrgnkx"
+
+    @Test
+    fun part1() {
+        val rules = Generator.Rules.PART1
+        val gA = Generator("A", 65, rules, true)
+        val gB = Generator("B", 8921, rules, true)
+        val judge = Judge(gA, gB)
+        assertEquals(1, judge.stepNTimes(5))
+
+
+        assertEquals(listOf(1092455L, 1181022009L, 245556042L, 1744312007L, 1352636452L), gA.rememberedValues.take(5))
+        assertEquals(listOf(430625591L, 1233683848L, 1431495498L, 137874439L, 285222916L), gB.rememberedValues.take(5))
+
+
+        val solution = Judge(Generator("A", input["A"]!!, rules), Generator("B", input["B"]!!, rules))
+        val result = solution.step40MillionTimes()
+        printSolution(15, 1, result, 612)
+    }
+
+    @Test
+    fun part2() {
+        val rules = Generator.Rules.PART2
+        val gA = Generator("A", 65, rules, true)
+        val gB = Generator("B", 8921, rules, true)
+        val judge = Judge(gA, gB)
+        assertEquals(1, judge.stepNTimes(1056))
+
+
+        assertEquals(listOf(1352636452L, 1992081072L, 530830436L, 1980017072L, 740335192L), gA.rememberedValues.take(5))
+        assertEquals(listOf(1233683848L, 862516352L, 1159784568L, 1616057672L, 412269392L), gB.rememberedValues.take(5))
+
+
+        val solution = Judge(Generator("A", input["A"]!!, rules), Generator("B", input["B"]!!, rules))
+        val result = solution.step5MillionTimes()
+        printSolution(15, 2, result, 285)
+    }
+}
+
+
+
